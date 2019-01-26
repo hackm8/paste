3c8f9198560d14bccc3ef4f770c5c8edb3f8835a
TheDevXen <thedevxen@gmail.com>
2019-01-26 23:02:24 +0530

    fix

--- routes/abuse.js
+++ routes/abuse.js
@@ -6,7 +6,7 @@ router.get('/:id', async (req, res) => {
     if (!req.params.id) return res.redirect('/');
     if (!req.cookies.username) return res.send('Please <a href="/auth">show</a> your identity before abusing anyone :)')
     request(
-        `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=400674061&parse_mode=Markdown&text=Abuse+Reported by ${req.cookies.username}\nhttps://p.hackmate.tech/c/${req.params.id}`,
+        `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=400674061&text=Abuse+Reported by ${req.cookies.username}\nhttps://p.hackmate.tech/c/${req.params.id}`,
         () => res.end('The code was reported to the admin!')
     )
 });
